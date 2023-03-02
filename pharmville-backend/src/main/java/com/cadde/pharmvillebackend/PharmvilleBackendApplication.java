a8f5bcdb4b3b57980e30a87ea7c06bd05fefc0f7
deniz-dilaverler <dmertdilaverler@gmail.com>
2023-03-02 23:24:19 +0300

    Backend Spring Boot project created

---
+++ pharmville-backend/src/main/java/com/cadde/pharmvillebackend/PharmvilleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cadde.pharmvillebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmvilleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmvilleBackendApplication.class, args);
+	}
+
+}
