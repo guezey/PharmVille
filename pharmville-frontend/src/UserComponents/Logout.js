5919c9fec1860f1eaae595747845ba1d4eb3e145
Dağhan Ünal <daghanunal20@gmail.com>
2023-06-03 21:05:07 +0300

    merge

--- pharmville-frontend/src/UserComponents/Logout.js
+++ pharmville-frontend/src/UserComponents/Logout.js
@@ -1,17 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const Logout = ({onLogout}) => {
-    const handleLogout = () => {
-        localStorage.removeItem("userRole");
-        localStorage.removeItem("userData");
-        onLogout();
-    };
-  
-    return (
-      <button onClick={handleLogout} className="logoutButton">
-        Logout
-      </button>
-    );
-};
+const Logout = ({ onLogout }) => (
+  <button onClick={onLogout} className="logout-button">
+    Logout
+  </button>
+);
 
 export default Logout;
